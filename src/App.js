17f9b454973cb60a05b26dce648e056bc42e2696
yaroslav <yaroslav7v@gmail.com>
2023-04-26 23:26:38 +0300

    first release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'},
+    ])
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', body: 'Description'},
+        {id: 2, title: 'Python 2', body: 'Description'},
+        {id: 3, title: 'Python 3', body: 'Description'},
+    ])
+
+    return (
+        <div className="App">
+            <PostList posts={posts} title={"List of posts 1"}/>
+            <PostList posts={posts2} title={"List of posts 2"}/>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PostItem = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <div className={"post"}>
+                <div className={"post-content"}>
+                    <strong>{props.post.id}. {props.post.title}</strong>
+                    <div>
+                        {props.post.body}
+                    </div>
+                </div>
+                <div className={"post-btns"}>
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
