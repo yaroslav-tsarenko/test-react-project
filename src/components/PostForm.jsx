4362d869d2c5280a74a8717ee7b9b4f6f14b56c6
yaroslav <yaroslav7v@gmail.com>
2023-04-28 23:38:31 +0300

    created component PostItem

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({title: '', body: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({title: '', body: ''})
+    }
+
+    return (
+        <div>
+            <form>
+                {/*Component manager*/}
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    type="text"
+                    placeholder="Name of post"/>
+
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    type="text"
+                    placeholder="Description of post"/>
+                <MyButton type="submit" onClick={addNewPost}>Create post</MyButton>
+            </form>
+        </div>
+    );
+};
+
+export default PostForm;
